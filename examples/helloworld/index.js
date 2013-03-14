6fe171e43d6be72bdc702a07adc783be3879926a
Colin O'Brien <cobrien@rallydev.com>
2013-03-13 21:59:02 -0400

    Basic API is done.  Now on to the doc site

--- examples/helloworld/index.js
+++ examples/helloworld/index.js
@@ -5,17 +5,20 @@ var app = interlude();
 var echoService = app.service("echo").version("1.0.0");
 
 var hello = function (req, res, next) {
-  res.send(req.params.message, 200);
+  res.json(req.params.message);
+  next();
 };
 
-echoService
-  .get()
-  .resource("hello")
-  .param("message")
+var loc = echoService.get()
   .version("1.0.0")
   .handler(hello)
   .summery("Echo's back the message that you send")
   .description("<p>This is a very long description that might be multi-lined.</p>" +
                "<p>Use html if you want.  It will get escaped out of the summery</p>");
 
-app.listen(8080);
+loc.resource("hello");
+loc.param("message").summery("About this parameter");
+
+app.listen(8080, function () {
+  console.log("Now listening on port 8080");
+});
