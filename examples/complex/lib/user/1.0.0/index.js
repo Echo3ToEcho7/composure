47f63d68953e05a32b52e839becd9e32df683489
Colin O'Brien <cobrien@rallydev.com>
2013-03-15 15:54:54 -0400

    Refactored 'Resource' to 'Endpoint'

--- examples/complex/lib/user/1.0.0/index.js
+++ examples/complex/lib/user/1.0.0/index.js
@@ -4,17 +4,17 @@ exports.load = function (service) {
   var loc = service.get()
     .summery("Location the use by UUID")
     .handler(function (req, res, next) {
-      res.send({ usename: "Hello!", id: req.params.id });
+      res.json({ usename: "Hello!", id: req.params.id });
       next();
     });
 
-  var param = loc.param(":id").summery("The uuid of the user");
+  var param = loc.param("id").summery("The uuid of the user");
 
 
   loc = service.post()
     .summery("Create a new user")
     .handler(function (req, res, next) {
-      res.send({ id: "00000-00000-000000000" });
+      res.json({ id: "00000-00000-000000000" });
       next();
     });
 };
--- examples/complex/lib/user/1.1.0/index.js
+++ examples/complex/lib/user/1.1.0/index.js
@@ -5,7 +5,7 @@ exports.load = function (service) {
   var loc = service.post()
     .summery("Create a new user")
     .handler(function (req, res, next) {
-      res.send({ id: "00001-00000-000000000", name: "World!" });
+      res.json({ id: "00001-00000-000000000", name: "World!" });
       next();
     });
 };
