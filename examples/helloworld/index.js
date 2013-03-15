5a8e6effd1a085d268a05cd4745462dac1b78dc2
Colin O'Brien <cobrien@rallydev.com>
2013-03-15 10:16:19 -0400

    Doc gen has begun

--- examples/helloworld/index.js
+++ examples/helloworld/index.js
@@ -2,7 +2,7 @@ var interlude = require("../../lib");
 
 var app = interlude();
 
-var echoService = app.service("echo").version("1.0.0");
+var echoService = app.name("Hello, World!").service("echo").version("1.0.0");
 
 var hello = function (req, res, next) {
   res.json(req.params.message);
@@ -17,7 +17,17 @@ var loc = echoService.get()
                "<p>Use html if you want.  It will get escaped out of the summery</p>");
 
 loc.resource("hello");
-loc.param("message").summery("About this parameter");
+loc.param("message").summery("The message that you want echoed back");
+
+loc = echoService.post()
+  .version("1.0.0")
+  .handler(hello)
+  .summery("Echo's back the message that you send (POST as an example)")
+  .description("<p>This is a very long description that might be multi-lined.</p>" +
+               "<p>Use html if you want.  It will get escaped out of the summery</p>");
+
+loc.resource("hello");
+loc.param("message").summery("The message that you want echoed back");
 
 app.listen(8080, function () {
   console.log("Now listening on port 8080");
